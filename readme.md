99b9b8e814c65429cf6242f1477bf808b5383914
montmorill <montmorill@outlook.com>
2026-07-09 18:22:18 +0800

    feat: update package.json, README, and add localization files; enhance plugin configuration and commands

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-fmtutils?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-fmtutils)
 
-
+为 Koishi 提供格式化输出工具
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,55 @@
-import { Context, Schema } from 'koishi'
+import type { Context } from 'koishi'
+import { h, Schema } from 'koishi'
+import enUS from '../locales/en-US.yml'
+import zhCN from '../locales/zh-CN.yml'
 
 export const name = 'fmtutils'
 
-export interface Config {}
+export interface Config {
+  markdown: boolean
+  tex: boolean
+  code: boolean
+  table: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  markdown: Schema.boolean().default(true).description('启用 markdown 指令。'),
+  tex: Schema.boolean().default(true).description('启用 TeX 指令。'),
+  code: Schema.boolean().default(true).description('启用代码块指令。'),
+  table: Schema.boolean().default(true).description('启用表格指令。'),
+})
 
 export function apply(ctx: Context, config: Config) {
-  // write your plugin here
+  ctx.i18n.define('en-US', enUS)
+  ctx.i18n.define('zh-CN', zhCN)
+
+  config.markdown && ctx.command('markdown <message:text>')
+    .action((_, message) => h('markdown', message))
+
+  config.tex && ctx.command('tex <message:text>')
+    .action((_, message) => h('markdown', `$$\n${message}\n$$`))
+
+  config.code && ctx.command('code <message:text>')
+    .option('lang', '-l <lang:string>')
+    .action(({ options }, message) =>
+      h('markdown', `\`\`\`${options?.lang || ''}\n${message}\n\`\`\``))
+
+  config.table && ctx.command('table <message:text>')
+    .option('virtual', '-v')
+    .option('transpose', '-T')
+    .action(({ options }, message) => {
+      let cells = message.split('\n').map(line => line.split(' '))
+      const maxLength = Math.max(...cells.map(row => row.length))
+      const columnCount = options?.transpose ? cells.length : maxLength
+      if (options?.transpose)
+        cells = Array.from({ length: maxLength }, (_, i) => cells.map(row => row[i]))
+
+      options?.virtual && cells.unshift([])
+      const lines = cells.map(row =>
+        `|${Array.from({ length: columnCount }, (_, index) =>
+          row[index]?.replaceAll('|', '\\|') ?? '').join('|')}|`)
+      lines.splice(1, 0, `${'|-'.repeat(columnCount)}|`)
+
+      return h('markdown', lines.join('\n'))
+    })
 }
